61da2584f92d174d721a4cba19701ec9bdff8a2a
priyanshu1258 <priyanshuattri05@gmail.com>
2025-04-17 13:03:35 +0530

    Delete axios.js

--- axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'; // Import axios for making HTTP requests
-
-const instance = axios.create({
-    baseURL:'http://127.0.0.1:5001/challenge-91b9f/us-central1/api'
-
-}); // Create an axios instance with a base URL
-
-    export default instance; // Export the axios instance for use in other files
